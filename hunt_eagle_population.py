df997ed0b95799b55f092c309de17cec8382a0a7
Eagle <eagle@eaglehunt.net>
2023-04-13 17:28:47 -0700

    new file

--- hunt_eagle_population.py
+++ hunt_eagle_population.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 """Simple script to calculate the estimated population after x years"""
 # Define the population variables
+
 CURRENT_US_POPULATION = 334_617_349
 BIRTH_RATE = 1/7
 DEATH_RATE = 1/13
@@ -19,20 +21,20 @@ def estimate_population(years):
     Returns:
         int: Estimated population after x amount of years
     """
-    # Calculate the total number of seconds in the given number of years
+    # Calculate seconds in given years
     total_seconds = years * DAYS_IN_YEAR * HOURS_IN_A_DAY * MINUTES_IN_AN_HOUR * SECONDS_IN_A_MINUTE
 
     # Calculate the number of births, deaths, and immigrants in the given number of seconds
-    total_births = total_seconds * BIRTH_RATE
-    total_deaths = total_seconds * DEATH_RATE
-    total_immigrants = total_seconds * IMMIGRANT_RATE
+    births = total_seconds * BIRTH_RATE
+    deaths = total_seconds * DEATH_RATE
+    immigrants = total_seconds * IMMIGRANT_RATE
 
     # Add the birth and immigration rates and subtracting the death rate
-    estimated_population = CURRENT_US_POPULATION + total_births + total_immigrants - total_deaths
+    estimated_population = CURRENT_US_POPULATION + births + immigrants - deaths
 
     # Round the population to the nearest integer and return the result rounded
     return round(estimated_population)
 
 # Get the input from the user and print the estimated population
 inputted_years = int(input("Enter the number of years: "))
-print(f"Estimated population after {inputted_years} years: {estimate_population(inputted_years):,}")
+print(f"Estimated population in {inputted_years} years: {estimate_population(inputted_years):,}")
